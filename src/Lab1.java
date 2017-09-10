147f2cfe883d69d42a8a87e3e442b5da2f9268f7
Tagonice <Tagon1228@gmail.com>
2017-09-10 12:08:52 +0200

    Change path and made an ugly solution for the switches in the beginning

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,13 +1,27 @@
 import TSim.*;
 
+import java.util.concurrent.Semaphore;
+
 public class Lab1 {
 
+  private TSimInterface tsi;
+
+  private AddingArrayList<Semaphore> semaphores;
+  private AddingArrayList<int[]> sensors;
+
   public Lab1(Integer speed1, Integer speed2) {
-    TSimInterface tsi = TSimInterface.getInstance();
+    tsi = TSimInterface.getInstance();
+
+    semaphores = new AddingArrayList<>();
+    sensors = new AddingArrayList<>();
 
     try {
       tsi.setSpeed(1,speed1);
       tsi.setSpeed(2, speed2);
+      tsi.setSwitch(17,7, TSimInterface.SWITCH_RIGHT);
+      tsi.setSwitch(15,9,TSimInterface.SWITCH_RIGHT);
+      //SensorEvent se = new SensorEvent(1,12,3,SensorEvent.ACTIVE );
+      //tsi.setSpeed(1,0);
     }
     catch (CommandException e) {
       e.printStackTrace();    // or only e.getMessage() for the error
