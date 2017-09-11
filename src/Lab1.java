662b13c4bff6127bf400d43dcb702be138c02ef4
Timmy Truong <Tagon1228@gmail.com>
2017-09-11 17:28:01 +0200

    Add action after train1 reaches station, chosen swtiches turn automatically
    Inverted our speed in train1 as soon as it reaches the south station, making it go backwards to the north station. Specific switches now turn automatically, making train1 able to clear map.

--- src/Lab1.java
+++ src/Lab1.java
@@ -27,17 +27,30 @@ public class Lab1 {
     @Override
     public void run(){
       try{
-        tsi.setSwitch(17,7,TSimInterface.SWITCH_RIGHT);
-        tsi.setSwitch(15,9,TSimInterface.SWITCH_RIGHT);
-        tsi.setSwitch(3,11,TSimInterface.SWITCH_RIGHT);
 
         tsi.setSpeed(id, speed);
         while(true){
           SensorEvent se = tsi.getSensor(id);
 
-          if((se.getXpos()==15) && (se.getYpos() == 13)){
+          if((se.getXpos()==14) && (se.getYpos()==7)){
+            tsi.setSwitch(17,7,TSimInterface.SWITCH_RIGHT);
+          }
+
+          if((se.getXpos()==19) && (se.getYpos()==9)){
+            tsi.setSwitch(15,9,TSimInterface.SWITCH_RIGHT);
+          }
+
+          if((se.getXpos()==1) && (se.getYpos()==10)){
+            tsi.setSwitch(3,11,TSimInterface.SWITCH_RIGHT);
+          }
+
+
+          if((se.getXpos()==15) && (se.getYpos()==13) || ((se.getXpos()==15) && (se.getYpos()==3))){
             if(se.getStatus() == SensorEvent.ACTIVE){
               tsi.setSpeed(se.getTrainId(), 0);
+              sleep(1000 + Math.abs(speed) * 15);
+              speed = -speed;
+              tsi.setSpeed(se.getTrainId(), speed);
             }
           }
         }
