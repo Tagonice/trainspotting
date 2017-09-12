5b21773c8e515d227e85e2aa1ee21dd9b528c2fe
Timmy Truong <Tagon1228@gmail.com>
2017-09-12 14:25:12 +0200

    Add all switches to sensors, manually set speed at start
    Assumed that all cases for the switches are thought out, although no semaphores are implented, therefore there might be some cases where switches won't turn automatically.
    By reaching a station, the speed parameter gets inverted and if a person;
    sets  one or both of the speed parameters to 0,starts the program, assign it a new speed manually, the program  will cause the speed to become 0 when reaching a station.

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,5 +1,7 @@
-import TSim.*;
-
+import TSim.AddingArrayList;
+import TSim.CommandException;
+import TSim.SensorEvent;
+import TSim.TSimInterface;
 import java.util.concurrent.Semaphore;
 
 
@@ -19,77 +21,94 @@ public class Lab1 {
     train2.start();
   }
 
-  public class Train extends Thread{
+  public class Train extends Thread {
+
     int id;
     int speed;
     boolean downward;
 
-    public Train(int id, int speed, boolean downward){
+    public Train(int id, int speed, boolean downward) {
       this.id = id;
       this.speed = speed;
       this.downward = downward;
     }
 
     @Override
-    public void run(){
-      try{
+    public void run() {
+      try {
 
-        tsi.setSpeed(id, speed);
-        while(true){
+        /*tsi.setSpeed(id, speed);*///Speed args are 15 and 15.
+        tsi.setSpeed(id, 0); // Prevent the speed to become 0 when reaching a station
+
+        while (true) {
           SensorEvent se = tsi.getSensor(id);
           //Coming from north
-          if((se.getXpos()==14) && (se.getYpos()==7) && downward){
-            tsi.setSwitch(17,7,TSimInterface.SWITCH_RIGHT);
-          }
 
-          if((se.getXpos()==19) && (se.getYpos()==9) && downward){
-            tsi.setSwitch(15,9,TSimInterface.SWITCH_RIGHT);
-          }
-
-          if((se.getXpos()==6) && (se.getYpos()==9) && downward) {
-            tsi.setSwitch(4, 9, TSimInterface.SWITCH_LEFT);
-            /*if(TSimInterface.switchDir != 0x01) {
-              tsi.setSwitch(4, 9, TSimInterface.SWITCH_LEFT);
-            }*/
-          }   //Trying to turn a switch that is already turned the right way
-          //Or so I thought, looks like passing the same switch twice makes it throw exception...
-
-          /*if((se.getXpos()==1) && (se.getYpos()==10) && downward) {
-            tsi.setSwitch(3, 11, TSimInterface.SWITCH_LEFT); //<--- Proves that you actually can turn an
-          }*/                                                //already turned right switch
-
-          if((se.getXpos()==1) && (se.getYpos()==10) && downward) {
-            tsi.setSwitch(3, 11, TSimInterface.SWITCH_LEFT);
-          }
-
-          //Coming from south
-          if((se.getXpos()==1) && (se.getYpos()==10) && !downward){
-            tsi.setSwitch(4, 9, TSimInterface.SWITCH_RIGHT);
-          }
-
-          //North station
-          if((se.getXpos()==15) && (se.getYpos()==5)){
-            if(se.getStatus() == SensorEvent.ACTIVE){
-              tsi.setSpeed(se.getTrainId(), 0);
+          if ((se.getStatus() == SensorEvent.ACTIVE)) {
+            if ((se.getXpos() == 14) && (se.getYpos() == 7) && downward) {
+              tsi.setSwitch(17, 7, TSimInterface.SWITCH_RIGHT);
             }
-          }
 
-          //South station
-          if((se.getXpos()==15) && (se.getYpos()==13) || ((se.getXpos()==15) && (se.getYpos()==3))){
-            if(se.getStatus() == SensorEvent.ACTIVE){
-              tsi.setSpeed(se.getTrainId(), 0);
-              sleep(1000 + Math.abs(speed) * 15);
-              speed = -speed;
-              tsi.setSpeed(se.getTrainId(), speed);
+            if ((se.getXpos() == 19) && (se.getYpos() == 9) && downward) {
+              tsi.setSwitch(15, 9, TSimInterface.SWITCH_RIGHT);
+            }
+
+            if ((se.getXpos() == 6) && (se.getYpos() == 9) && downward) {
+              tsi.setSwitch(4, 9, TSimInterface.SWITCH_LEFT);
+            }
+
+            if ((se.getXpos() == 15) && (se.getYpos() == 8) && downward) {
+              tsi.setSwitch(17, 7, TSimInterface.SWITCH_LEFT);
+            }
+
+            if ((se.getXpos() == 1) && (se.getYpos() == 10) && downward) {
+              tsi.setSwitch(3, 11, TSimInterface.SWITCH_RIGHT);
+            }
+
+            //Coming from south
+            if ((se.getXpos() == 1) && (se.getYpos() == 10) && !downward) {
+              tsi.setSwitch(4, 9, TSimInterface.SWITCH_RIGHT);
+            }
+
+            if ((se.getXpos() == 13) && (se.getYpos() == 10) && !downward) {
+              tsi.setSwitch(15, 9, TSimInterface.SWITCH_LEFT);
+            }
+
+            if ((se.getXpos() == 5) && (se.getYpos() == 11) && !downward) {
+              tsi.setSwitch(3, 11, TSimInterface.SWITCH_LEFT);
+            }
+
+            if ((se.getXpos() == 19) && (se.getYpos() == 9) && !downward) {
+              tsi.setSwitch(17, 7, TSimInterface.SWITCH_LEFT);
+            }
+
+            //North station
+            if ((se.getXpos() == 15) && ((se.getYpos() == 5) || se.getYpos() == 3)) {
+              if (se.getStatus() == SensorEvent.ACTIVE) {
+                tsi.setSpeed(se.getTrainId(), 0);
+                downward = true;
+                sleep(1000 + Math.abs(speed) * 15);
+                speed = -speed;
+                tsi.setSpeed(se.getTrainId(), speed);
+              }
+            }
+
+            //South station
+            if ((se.getXpos() == 15) && ((se.getYpos() == 11) || se.getYpos() == 13)) {
+              if (se.getStatus() == SensorEvent.ACTIVE) {
+                tsi.setSpeed(se.getTrainId(), 0);
+                downward = false;
+                sleep(1000 + Math.abs(speed) * 15);
+                speed = -speed;
+                tsi.setSpeed(se.getTrainId(), speed);
+              }
             }
           }
         }
-      }
-      catch (CommandException e){
+      } catch (CommandException e) {
         e.printStackTrace();
         System.exit(1);
-      }
-      catch (InterruptedException e){
+      } catch (InterruptedException e) {
         e.printStackTrace();
         System.exit(1);
       }
